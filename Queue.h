e5bbfb8536533672bbbf610048d156f95bdc487f
Bowen117 <68262810+Bowen117@users.noreply.github.com>
2020-07-23 03:34:54 +0800

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,27 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+#include "Places.h"
+/////////////////////////////
+
+//make new queue
+struct Queue* NewQueue(int capacity);
+
+//check if Queue is full.
+// Queue is full when size becomes
+// equal to the capacity
+int isFull(struct Queue* queue);
+
+// Queue is empty when size is 0
+int isEmpty(struct Queue* queue);
+
+// Function to add an item to the queue.
+// It changes rear and size
+void enqueue(struct Queue* queue, PlaceId p);
+
+// Function to remove an item from queue.
+// It changes front and size
+int dequeue(struct Queue* queue);
+
+// Check if place is already in to-visit queue.
+int isNotInQueue (struct Queue* queue, PlaceId p);
